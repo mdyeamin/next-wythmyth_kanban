fdf89e6763a85b9d64b4fa679add6816d0f69ebe
md yeamin <ahmedyeamin05@gmail.com>
2025-12-06 11:37:33 +0600

    kanvan board for hythmyth

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello there!</h1>
+    <div className="">
+      <h1 className="text-4xl">Hello there!</h1>
     </div>
   );
 }
